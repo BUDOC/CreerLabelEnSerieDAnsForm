38fdd2773fa983e7e09b4e51a9a278e1b68737c7
Budoc53 GG <viot.g.53@orange.fr>
2021-01-18 17:29:23 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreerLabelEnSerieDAnsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string T;
+            int cpt = 0;
+            for (int j = 0; j < 4; j++)
+            {
+
+                for (int i = 0; i < 3; i++)
+                {
+                    cpt++;
+                    Label L = new System.Windows.Forms.Label();
+                    L.Parent = this;
+                    //  L.Click += new EventHandler(Label_Click);
+                    T = "L" + cpt.ToString();
+                    L.Text = T;
+                    L.Name = T;
+                    L.Width = 30;
+                    L.Height = 20;
+                    L.Top = 20 + j * 40;
+                    L.Left = 20 + i * 40;
+                    L.Show();
+                }
+                cpt++;
+            }
+        }
+    }
+}
